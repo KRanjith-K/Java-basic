075f1b4d398058c4df7c66b017c0fd315c5394d6
KRanjith-K <kranjithroy@gmail.com>
2026-02-20 22:42:38 +0530

    Add program to find the largest of three numbers

---
+++ largest3number.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class largestof3number {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+
+        if(num1 > num2 && num1 > num3){
+            System.out.println("Largest number is num1 " + num1);
+        }
+        else if(num2 > num1 && num2 > num3){
+            System.out.println("Largest number is num2 " + num2);
+        }
+        else{
+            System.out.println("Largest number is num3 " + num3);
+        }
+    }
+}
